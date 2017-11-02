436549182d932b4b1b4fdea927b692a84b8a35a3
Alistair Kung <alistaircckung@gmail.com>
2017-11-02 15:30:21 +0000

    added simplecov for test coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,17 @@ require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
 
+require 'simplecov'
+require 'simplecov-console'
+
 Capybara.app = App
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
+  # Want a nice code coverage website? Uncomment this next line!
+  # SimpleCov::Formatter::HTMLFormatter
+])
+SimpleCov.start
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
